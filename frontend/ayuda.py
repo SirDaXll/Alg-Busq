078888a152bca3ff6ffa810d6b98ea663c25974a
El Diego <SirDaXll@users.noreply.github.com>
2023-06-19 20:12:50 -0400

    si

--- frontend/ayuda.py
+++ frontend/ayuda.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Ayuda = QtWidgets.QWidget()
-    ui = Ui_Ayuda()
+    ui = Ayuda()
     ui.setupUi(Ayuda)
     Ayuda.show()
     sys.exit(app.exec())
